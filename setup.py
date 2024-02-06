f8f8a7fcc080f87e8ec896c40d5a9727102d997d
Pranjal Pandit <pranjal5215@gmail.com>
2024-02-06 15:15:17 +0530

    push for data chnages in setup

--- setup.py
+++ setup.py
@@ -9,17 +9,17 @@ classifiers = [
 ]
  
 setup(
-  name='convert_numbers',
-  version='0.4',
-  description='Arabic Persian English Hindi Numbers is a Python library to convert numbers from-to Arabic, Hinid, Persian and English.',
+  name='convert_indian_numbers',
+  version='0.5',
+  description='Arabic Persian English Hindi Numbers is a Python library to convert numbers from-to Arabic, Indian Hindi, Persian and English.',
   long_description=open('README.md').read(),
   long_description_content_type="text/markdown",
-  url='https://github.com/azwri/convert_numbers',  
+  url='https://github.com/pranjal5215/convert_numbers',  
   author='Azwri',
-  author_email='aazwri@gmail.com',
+  author_email='pranjal5215@gmail.com',
   license='MIT', 
   classifiers=classifiers,
-  keywords='Arabic Persian English Hindi Numbers Python conversion numbers from-to Arabic, Hinid, Persian and English', 
+  keywords='Arabic Persian English Indian Hindi Numbers Python conversion numbers from-to Arabic, Indian Hindi, Persian and English', 
   packages=find_packages(),
   install_requires=['']
 )
